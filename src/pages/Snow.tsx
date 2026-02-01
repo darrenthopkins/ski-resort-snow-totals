b2dc307f80dd085623211add2b7fa30c151061d1
Darren Hopkins <dhopkins@felicias-air.myfiosgateway.com>
2026-01-31 19:17:10 -0500

    Rename Tab1 to Snow and update tab routing

--- src/pages/Snow.tsx
+++ src/pages/Snow.tsx
@@ -1,6 +1,6 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import './Snow.css';
 
 const Tab1: React.FC = () => {
   return (
